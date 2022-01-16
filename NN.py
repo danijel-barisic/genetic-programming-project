add152de45c2e0840d0408f864db9c99ff82e98a
Danijel Barišić <danijel.bar17@gmail.com>
2022-01-16 01:08:20 +0100

    Upgraded neural network + PSO (semi-working)

--- NN.py
+++ NN.py
@@ -1,28 +1,14 @@
-
-# INSTRUCTIONS for running neural network algorithm for protein warriors:
-# 1) In config.json put INPUT_COUNT=2 (eating opponents is disabled for neural network, due complexity and slowness)
-# 2) Run ProteinWarriorsNoEating.py
-
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
+import pyswarms as ps
 
 from Algorithm import Algorithm
-import json
-from pso.optimizer import Optimizer
 
-# Figure out how pso fits into the algorithm (whether you remember history, or you do it dynamically, or something
-#   completely else that depends on NN's or sumn.)
-# Figure out WHEN to apply pso, how to select the fittest (it's in evolve_population I think) etc.
-# Finish the evaluate function of the unit, determine the appropriate args n arg shape, perhaps it's simply the input values,
-# or no.
-# Determine input array shapes by backtracking NN interface.
-
-with open('./config.json') as f:
-    config = json.load(f)
-
-MAXIMUM_SPEED = config["MAXIMUM_SPEED"]
 LOSS = 'mse'
+
+HIDDEN_COUNT1 = 5
+HIDDEN_COUNT2 = 3
 
 
 class Unit:
@@ -33,82 +19,92 @@ class Unit:
         self.input_values = []
         self.output_values = []
         self.fitness = 0
-        # self.model = Sequential()
-        # self.create_network()
-        # self.train_samples = []  # samples (tuple of: input tuple of size input_count,
-        # and output tuple of size output_count). This is history of movement n calculations of right moves.
-        # self.x_train = []  # np.zeros((300,2))
-        # self.y_train = []  # np.zeros((300,1))
-        self.arr = np.zeros((2, 1))
+        self.model = Sequential()
+        self.create_network()
 
-    # def create_network(self):
-    #     # self.model = Sequential()
-    #     self.model.add(Dense(self.input_count, activation='relu', input_dim=2, name="Dense1"))
-    #     # # self.model.add(Dense(3, activation='relu'))
-    #     # self.model.add(Input())
-    #     self.model.add(Dense(self.output_count, activation='sigmoid', name="Dense2"))
-    #
-    #     # self.model.add(Dense(4, activation='sigmoid', input_shape=(2,), use_bias=True))
-    #     # self.model.add(Dense(4, activation='sigmoid', use_bias=True))
-    #     # self.model.add(Dense(3, activation='softmax', use_bias=True))
-    #     self.model.compile(optimizer='adam', loss=LOSS,
-    #                        metrics=['mae'])  # TODO maybe other optimizer n loss, n do I need metrics?
-    #     # or self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    def create_network(self):
+        self.model.add(Dense(HIDDEN_COUNT1, activation='relu', input_dim=self.input_count, name="Dense1",
+                             kernel_initializer='random_normal', bias_initializer='zeros'))
+        self.model.add(Dense(HIDDEN_COUNT2, activation='sigmoid', name="Dense2",
+                             kernel_initializer='random_normal', bias_initializer='zeros'))
+        self.model.add(Dense(self.output_count, activation='sigmoid', name="Dense3",
+                             kernel_initializer='random_normal', bias_initializer='zeros'))
+
+        self.model.compile(optimizer='adam', loss=LOSS,
+                           metrics=['mae'])
 
     def evaluate(self):
+        # model.predict is the method responsible for calculating output value on your weighted NN depending on input
 
-        # model.predict is the thing responsible for calculating output value on yo weighted NN applied on test data
-        # (or even on train data, it's all data after all, but it makes sense to test on data that's not identical to
-        # train data, to see that it really do be working
-        # self.model.predict(....)
-
-        correct_output = (MAXIMUM_SPEED, self.input_values[1])
-
-        # self.x_train= np.append(self.x_train,tuple(self.input_values))
-        # self.y_train = np.append(self.y_train,correct_output)
-        # self.train_samples.append(())
-
-        # self.x_train.append(tuple(self.input_values))
-        # self.y_train.append(correct_output)
-
-        self.nn.x_train.append(tuple(self.input_values))
-        self.nn.y_train.append(correct_output)
-
-        x = [[self.input_values[0], self.input_values[
-            1]]]  # inner array is first sample, second inner array if it existed would be second sample,
-        # members of the array are features, there are 2 features, angle n speed
-        # for input_val in self.input_values:
-        #     y.append([input_val[0],input_val[1]])
-
-        # self.output_values = self.model.predict(self.arr) #error
-        self.arr = np.array(x)
-
-        y_pred = self.nn.model(self.arr) # shorthand for predict
+        y_pred = self.model.predict(np.array([self.input_values]))
         pred = list()
-        for i in range(len(y_pred)):
-            pred.append((np.argmax(y_pred[i])))
+        for i in range(y_pred.size):
+            pred.append(y_pred[0][i])
 
-        pred.append(MAXIMUM_SPEED)
         self.output_values = pred
 
     def __lt__(self, other):
         return self
 
 
+def fitness_func(swarm, **kwargs):
+    """Method for unit/particle(?) fitness
+
+    Inputs
+    ------
+    swarm: numpy.ndarray of shape (n_particles, dimensions)
+        The swarm that will perform the search
+
+    Returns
+    -------
+    numpy.ndarray of shape (n_particles, )
+        The computed fitness for each particle
+    """
+
+    unit = kwargs["the_unit"]
+
+    n_particles = swarm.shape[0]
+
+    # negative fitness because pyswarms optimises for minimum
+    return np.array([-unit.fitness] * n_particles)
+
+
+def format_flat_weights(flat_weights, input_count, output_count):
+    result_list = []
+
+    tmp_array = np.empty((input_count, HIDDEN_COUNT1), dtype=np.float32)
+
+    for i in range(input_count):
+        tmp_array[i] = [flat_weights[i * HIDDEN_COUNT1 + j] for j in range(HIDDEN_COUNT1)]
+
+    result_list.append(tmp_array)
+    result_list.append(np.array([0.] * HIDDEN_COUNT1, dtype=np.float32))  # bias
+
+    tmp_array = np.empty((HIDDEN_COUNT1, HIDDEN_COUNT2), dtype=np.float32)
+    init_index = input_count * HIDDEN_COUNT1  # + HIDDEN_COUNT1
+
+    for i in range(HIDDEN_COUNT1):
+        tmp_array[i] = [flat_weights[init_index + i * HIDDEN_COUNT2 + j] for j in range(HIDDEN_COUNT2)]
+
+    result_list.append(tmp_array)
+    result_list.append(np.array([0.] * HIDDEN_COUNT2, dtype=np.float32))  # bias
+
+    tmp_array = np.empty((HIDDEN_COUNT2, output_count), dtype=np.float32)
+    init_index = init_index + HIDDEN_COUNT1 * HIDDEN_COUNT2  # + HIDDEN_COUNT2
+
+    for i in range(HIDDEN_COUNT2):
+        tmp_array[i] = [flat_weights[init_index + i * output_count + j] for j in range(output_count)]
+
+    result_list.append(tmp_array)
+    result_list.append(np.array([0.] * output_count, dtype=np.float32))  # bias
+
+    return result_list
+
+
 class NN(Algorithm):
 
     def __init__(self, input_count, output_count, population_size):
         Algorithm.__init__(self, input_count, output_count, population_size)
-        self.model = Sequential()
-        self.create_network()
-        self.x_train = []
-        self.y_train = []
-
-    def create_network(self):
-        self.model.add(Dense(self.input_count, activation='relu', input_dim=2, name="Dense1"))
-        self.model.add(Dense(self.output_count, activation='sigmoid', name="Dense2"))
-        self.model.compile(optimizer='adam', loss=LOSS,
-                           metrics=['mae'])
 
     # function which returns a randomly generated population
     def create_population(self):
@@ -117,43 +113,27 @@ class NN(Algorithm):
             population.append(Unit(self.input_count, self.output_count, self))
         return population
 
-    # # function which takes current population as a parameter, and returns evolved population
-    # def evolve_population(self, population):
-    #
-    #     for i,unit in enumerate(population):
-    #         pso = Optimizer(model=unit.model,
-    #                         loss=LOSS,
-    #                         n=5,  # Number of particles
-    #                         acceleration=1.0,  # Contribution of recursive particle velocity (acceleration)
-    #                         local_rate=0.6,  # Contribution of locally best weights to new velocity
-    #                         global_rate=0.4)  # Contribution of globally best weights to new velocity
-    #
-    #         # print("Training unit " + str(i+1) +": ")
-    #         # Train model on provided data
-    #         pso.fit(unit.x_train, unit.y_train, steps=10, batch_size=20)
-    #
-    #     for unit in population:
-    #         unit.x_train.clear()
-    #         unit.y_train.clear()
-    #
-    #     return population
-
     # function which takes current population as a parameter, and returns evolved population
     def evolve_population(self, population):
 
-        pso = Optimizer(model=self.model,
-                        loss=LOSS,
-                        n=5,  # Number of particles
-                        acceleration=1.0,  # Contribution of recursive particle velocity (acceleration)
-                        local_rate=0.6,  # Contribution of locally best weights to new velocity
-                        global_rate=0.4)  # Contribution of globally best weights to new velocity
+        # PSO optimizes a function, and our NN represents one such function, mapping inputs (however many)
+        # to outputs (currently 2 outputs). Each particle is more or less a list containing as many arguments
+        # as there are dimensions, which are real numbers
+        # representing individual weights of neurons in the NN, hence being a description of the NN.
 
-        # print("Training unit " + str(i+1) +": ")
-        # Train model on provided data
-        pso.fit(self.x_train, self.y_train, steps=20, batch_size=50)
+        # Initialize swarm
+        options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
 
-        self.x_train.clear()
-        self.y_train.clear()
+        # Call instance of PSO
+        dimensions = self.input_count * HIDDEN_COUNT1 + HIDDEN_COUNT1 * HIDDEN_COUNT2 + \
+                     HIDDEN_COUNT2 * self.output_count
+
+        optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=dimensions, options=options)
+
+        for unit in population:
+            # Perform optimization
+            cost, pos = optimizer.optimize(fitness_func, iters=1000, the_unit=unit, )
+            unit.model.set_weights(format_flat_weights(pos, unit.input_count, unit.output_count))
 
         return population
 
