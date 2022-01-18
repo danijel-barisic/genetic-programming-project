2c37f8b5ea8b1b2ae7b55f5b87a18799298bbd21
Danijel Barišić <danijel.bar17@gmail.com>
2022-01-18 19:29:49 +0100

    Finished neural network + PSO algorithm

--- NN.py
+++ NN.py
@@ -5,8 +5,6 @@ import pyswarms as ps
 
 from Algorithm import Algorithm
 
-LOSS = 'mse'
-
 HIDDEN_COUNT1 = 5
 HIDDEN_COUNT2 = 3
 
@@ -30,12 +28,10 @@ class Unit:
         self.model.add(Dense(self.output_count, activation='sigmoid', name="Dense3",
                              kernel_initializer='random_normal', bias_initializer='zeros'))
 
-        self.model.compile(optimizer='adam', loss=LOSS,
+        self.model.compile(optimizer='adam', loss='mse',
                            metrics=['mae'])
 
     def evaluate(self):
-        # model.predict is the method responsible for calculating output value on your weighted NN depending on input
-
         y_pred = self.model.predict(np.array([self.input_values]))
         pred = list()
         for i in range(y_pred.size):
@@ -47,29 +43,7 @@ class Unit:
         return self
 
 
-def fitness_func(swarm, **kwargs):
-    """Method for unit/particle(?) fitness
-
-    Inputs
-    ------
-    swarm: numpy.ndarray of shape (n_particles, dimensions)
-        The swarm that will perform the search
-
-    Returns
-    -------
-    numpy.ndarray of shape (n_particles, )
-        The computed fitness for each particle
-    """
-
-    unit = kwargs["the_unit"]
-
-    n_particles = swarm.shape[0]
-
-    # negative fitness because pyswarms optimises for minimum
-    return np.array([-unit.fitness] * n_particles)
-
-
-def format_flat_weights(flat_weights, input_count, output_count):
+def convert_flat_weights_to_nn_format(flat_weights, input_count, output_count):
     result_list = []
 
     tmp_array = np.empty((input_count, HIDDEN_COUNT1), dtype=np.float32)
@@ -78,27 +52,40 @@ def format_flat_weights(flat_weights, input_count, output_count):
         tmp_array[i] = [flat_weights[i * HIDDEN_COUNT1 + j] for j in range(HIDDEN_COUNT1)]
 
     result_list.append(tmp_array)
-    result_list.append(np.array([0.] * HIDDEN_COUNT1, dtype=np.float32))  # bias
+    result_list.append(np.array([0.] * HIDDEN_COUNT1, dtype=np.float32))
 
     tmp_array = np.empty((HIDDEN_COUNT1, HIDDEN_COUNT2), dtype=np.float32)
-    init_index = input_count * HIDDEN_COUNT1  # + HIDDEN_COUNT1
+    init_index = input_count * HIDDEN_COUNT1
 
     for i in range(HIDDEN_COUNT1):
         tmp_array[i] = [flat_weights[init_index + i * HIDDEN_COUNT2 + j] for j in range(HIDDEN_COUNT2)]
 
     result_list.append(tmp_array)
-    result_list.append(np.array([0.] * HIDDEN_COUNT2, dtype=np.float32))  # bias
+    result_list.append(np.array([0.] * HIDDEN_COUNT2, dtype=np.float32))
 
     tmp_array = np.empty((HIDDEN_COUNT2, output_count), dtype=np.float32)
-    init_index = init_index + HIDDEN_COUNT1 * HIDDEN_COUNT2  # + HIDDEN_COUNT2
+    init_index = init_index + HIDDEN_COUNT1 * HIDDEN_COUNT2
 
     for i in range(HIDDEN_COUNT2):
         tmp_array[i] = [flat_weights[init_index + i * output_count + j] for j in range(output_count)]
 
     result_list.append(tmp_array)
-    result_list.append(np.array([0.] * output_count, dtype=np.float32))  # bias
+    result_list.append(np.array([0.] * output_count, dtype=np.float32))
 
     return result_list
+
+
+def fitness_func(swarm, **kwargs):
+    population = kwargs["population"]
+    n_particles = len(population)
+
+    res = np.empty(shape=n_particles)
+
+    for i, unit in enumerate(population):
+        res[i] = -unit.fitness
+
+    # The computed fitness for each particle.
+    return res
 
 
 class NN(Algorithm):
@@ -106,38 +93,29 @@ class NN(Algorithm):
     def __init__(self, input_count, output_count, population_size):
         Algorithm.__init__(self, input_count, output_count, population_size)
 
-    # function which returns a randomly generated population
     def create_population(self):
         population = []
         for _ in range(self.population_size):
             population.append(Unit(self.input_count, self.output_count, self))
         return population
 
-    # function which takes current population as a parameter, and returns evolved population
     def evolve_population(self, population):
 
-        # PSO optimizes a function, and our NN represents one such function, mapping inputs (however many)
-        # to outputs (currently 2 outputs). Each particle is more or less a list containing as many arguments
-        # as there are dimensions, which are real numbers
-        # representing individual weights of neurons in the NN, hence being a description of the NN.
-
-        # Initialize swarm
         options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
 
-        # Call instance of PSO
         dimensions = self.input_count * HIDDEN_COUNT1 + HIDDEN_COUNT1 * HIDDEN_COUNT2 + \
                      HIDDEN_COUNT2 * self.output_count
 
-        optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=dimensions, options=options)
+        optimizer = ps.single.GlobalBestPSO(n_particles=self.population_size, dimensions=dimensions, options=options)
 
-        for unit in population:
-            # Perform optimization
-            cost, pos = optimizer.optimize(fitness_func, iters=1000, the_unit=unit, )
-            unit.model.set_weights(format_flat_weights(pos, unit.input_count, unit.output_count))
+        stats = optimizer.optimize(fitness_func, iters=1000, population=population, )
+
+        for i, unit in enumerate(population):
+            pos = optimizer.swarm.position[i]
+            unit.model.set_weights(convert_flat_weights_to_nn_format(pos, unit.input_count, unit.output_count))
 
         return population
 
-    # function which takes a unit and its inputs as parameters, and returns the values that unit produces
     def calculate_values(self, unit, inputs):
         if len(inputs) != self.input_count:
             raise Exception
@@ -147,6 +125,6 @@ class NN(Algorithm):
         for input in inputs:
             unit.input_values.append(input)
 
-        unit.evaluate()  # calculate output values for the unit I believe, by using .predict( ... ) on model
+        unit.evaluate()
 
-        return tuple(unit.output_values)  # necessary for game movement I believe
+        return tuple(unit.output_values)
